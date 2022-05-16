3cdb1685c33784692fe4db475b971ec026a0cfc6
陈赞 <1010299303@qq.com>
2022-05-17 00:50:51 +0800

    【API集成】集成百度翻译-单词翻译api,接口调通

---
+++ app/src/main/java/com/zanchen/develop/wordsdiary/util/translateUtil/TranslateUtil.java
@@ -0,0 +1,35 @@
+package com.zanchen.develop.wordsdiary.util.translateUtil;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class TranslateUtil {
+    private static final String TRANS_API_HOST = "http://api.fanyi.baidu.com/api/trans/vip/translate";
+
+
+    public String getTransResult(String query, String from, String to) {
+        Map<String, String> params = buildParams(query, from, to);
+        return HttpGet.get(TRANS_API_HOST, params);
+    }
+
+    private Map<String, String> buildParams(String query, String from, String to) {
+        Map<String, String> params = new HashMap<String, String>();
+        params.put("q", query);
+        params.put("from", from);
+        params.put("to", to);
+
+        String appid = "20220509001209521";
+        params.put("appid", appid);
+
+        // 随机数
+        String salt = String.valueOf(System.currentTimeMillis());
+        params.put("salt", salt);
+
+        // 签名
+        String securityKey = "6VVVLofrNuOAwzZnCKwb";
+        String src = appid + query + salt + securityKey; // 加密前的原文
+        params.put("sign", MD5.md5(src));
+
+        return params;
+    }
+}
