a99118602905339f223c5dfcf2a8f33fa9b87257
陈赞 <1010299303@qq.com>
2022-05-18 01:25:52 +0800

    【新增代码】单词搜索功能实现

--- app/src/main/java/com/zanchen/develop/wordsdiary/fragment/study/StudyFragment.java
+++ app/src/main/java/com/zanchen/develop/wordsdiary/fragment/study/StudyFragment.java
@@ -4,6 +4,8 @@ import android.annotation.SuppressLint;
 import android.app.Notification;
 import android.content.Context;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -23,9 +25,17 @@ import androidx.lifecycle.ViewModelProvider;
 import androidx.navigation.Navigation;
 import androidx.navigation.fragment.NavHostFragment;
 
+import com.alibaba.fastjson.JSONObject;
+import com.zanchen.develop.wordsdiary.database.entity.Dictionary;
 import com.zanchen.develop.wordsdiary.databinding.FragmentStudyBinding;
-import com.zanchen.develop.wordsdiary.fragment.study.studyInfo.StudyInfoFragment;
-import com.zanchen.develop.wordsdiary.util.translateUtil.TranslateUtil;
+
+import java.util.Objects;
+
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 
 public class StudyFragment extends Fragment {
 
@@ -40,6 +50,8 @@ public class StudyFragment extends Fragment {
     private SearchView searchView;
     //定义上下文
     private Context mContext;
+    //词典
+    public Dictionary dictionary = new Dictionary();
 
 
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -101,14 +113,45 @@ public class StudyFragment extends Fragment {
         action = StudyFragmentDirections.actionNavigationStudyToStudyInfoFragment();
         action.setTestString(s);
         Navigation.findNavController(view).navigate(action);
+        Log.d(TAG,"dictionary: " + dictionary.getWordname());
     };
 
     public SearchView.OnQueryTextListener onQueryText = new SearchView.OnQueryTextListener() {
         //单击搜索按钮时激发该方法
         @Override
-        public boolean onQueryTextSubmit(String query) {
-            Toast toast = Toast.makeText(mContext,"功能正在开发中！",Toast.LENGTH_LONG);
-            toast.show();
+        public boolean onQueryTextSubmit(String wordname) {
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    try{
+                        JSONObject json = new JSONObject();
+                        json.put("wordname",wordname);
+                        //http客户端
+                        OkHttpClient client = new OkHttpClient();
+                        //http请求
+                        Request request = new Request.Builder()
+                                .url("http://192.168.1.34:8080/dictionary/queryWords")
+                                .post(RequestBody.create(MediaType.parse("application/json"),json.toString()))
+                                .build();
+                        //执行发送
+                        Response response = client.newCall(request).execute();
+                        Log.d(TAG,"发送成功！");
+                        String result = null;
+                        if(response.body() != null){
+                            result = Objects.requireNonNull(response.body()).string();
+                        }
+                        Log.d(TAG,"result: " + result);
+                        Message message = Message.obtain();
+                        message.obj = result;
+                        handler.sendMessage(message);
+                    }catch (Exception e){
+                        e.printStackTrace();
+                        Log.d(TAG,"网络连接失败！");
+                        Toast toast = Toast.makeText(mContext,"网络连接失败！",Toast.LENGTH_LONG);
+                        toast.show();
+                    }
+                }
+            }).start();
             return false;
         }
         //用户输入时激发该方法
@@ -117,6 +160,18 @@ public class StudyFragment extends Fragment {
             return false;
         }
     };
+
+    private final Handler handler = new Handler(msg -> {
+        String s = msg.obj.toString();
+        JSONObject jsonObject = JSONObject.parseObject(s);
+        Log.d(TAG,"UI: " + jsonObject);
+        dictionary.setWid((int) Objects.requireNonNull(jsonObject.get("wid")));
+        dictionary.setWordname((String) Objects.requireNonNull(jsonObject.get("wordname")));
+        dictionary.setWordpronounce_UK((String) Objects.requireNonNull(jsonObject.get("wordpronounce_UK")));
+        dictionary.setWordpronounce_US((String) Objects.requireNonNull(jsonObject.get("wordpronounce_US")));
+        dictionary.setWordmean_CN((String) Objects.requireNonNull(jsonObject.get("wordmean_CN")));
+        return false;
+    });
 
 
     @Override
