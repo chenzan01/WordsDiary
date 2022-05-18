418249fcb9d7661ea9a8f881cb21d845119de794
陈赞 <1010299303@qq.com>
2022-05-18 21:00:42 +0800

    【新增代码】添加词典Fragment

---
+++ app/src/main/java/com/zanchen/develop/wordsdiary/fragment/dictionary/DictionaryViewModel.java
@@ -0,0 +1,7 @@
+package com.zanchen.develop.wordsdiary.fragment.dictionary;
+
+import androidx.lifecycle.ViewModel;
+
+public class DictionaryViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
